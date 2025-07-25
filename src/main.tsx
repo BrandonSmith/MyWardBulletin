153353b327ec76448ee96f9233360d24d293f824
Matthew <madofuller@gmail.com>
2025-07-25 08:28:48 -0500

    addig vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,11 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from './lib/SessionContext';
+import { inject } from '@vercel/analytics';
 
 const queryClient = new QueryClient();
+
+inject();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
